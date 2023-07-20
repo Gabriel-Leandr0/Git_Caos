5f0c7c30cdb5ad987f6ce7bb5a75b5234ca87afd
Ryan Capurso <ryan_capurso@hotmail.com>
2023-07-20 14:09:37 -0300

    Revert "Mais código inútil python"
    This reverts commit ac39b2979aa6b453f0669f303d73b5d8954ae2a8.

--- oi.py
+++ oi.py
@@ -1,8 +1,3 @@
 nomes = ['Ryan', 'Gabriel', 'Diogo', 'Lucas']
 idade_aleatoria = 11
-pontos= [(3,1),(5,6)]
-
-nome_do_usuario = input("Qual o seu nome?")
-print(f"O seu nome é: {nome_do_usuario}")
-print(f"O seu nome em maiúsculo é: {nome_do_usuario.upper()}")
-print(f"O seu nome em minúsculo é: {nome_do_usuario.lower()}")
+pontos=[(3,1),(5,6)]
