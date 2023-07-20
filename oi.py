ac39b2979aa6b453f0669f303d73b5d8954ae2a8
Lucas Gomes Cruz <lucas.cruz@modalgr.com.br>
2023-07-20 12:54:44 -0300

    Mais código inútil python

--- oi.py
+++ oi.py
@@ -1,3 +1,8 @@
 nomes = ['Ryan', 'Gabriel', 'Diogo', 'Lucas']
 idade_aleatoria = 11
-pontos=[(3,1),(5,6)]
+pontos= [(3,1),(5,6)]
+
+nome_do_usuario = input("Qual o seu nome?")
+print(f"O seu nome é: {nome_do_usuario}")
+print(f"O seu nome em maiúsculo é: {nome_do_usuario.upper()}")
+print(f"O seu nome em minúsculo é: {nome_do_usuario.lower()}")
